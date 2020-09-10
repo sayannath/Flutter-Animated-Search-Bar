2356883112cfc3174e9fa7d0fec2a40a52926594
sayannath <sayannath235@gmail.com>
2020-09-10 19:33:14 +0530

    Pushed

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:animated_search/search_bar/default_app_bar.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DefaultAppBar(),
+              body: Center(child: Text('Search')), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
